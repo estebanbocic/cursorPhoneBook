054954522002605f317af24f1ac3d1c49a6138e9
Esteban Bocic <ebocic@Estebans-MacBook-Air.local>
2017-03-08 23:23:05 -0300

    buscador validador de tel y rut OK

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 .controller('listRegionesComunasCtrl', function($scope, $http) {
   $http.get('https://private-4b730d-phonebooktest.apiary-mock.com/region').then(function(result){
-    console.log(result.data); // console feedback
+    //console.log(result.data); // console feedback
     $scope.regiones = result.data; // UI data
   })
 
@@ -9,8 +9,54 @@ angular.module('starter.controllers', [])
 })
 .controller('listPersonasCtrl', function ($scope, $http) {
   $http.get('https://private-4b730d-phonebooktest.apiary-mock.com/persona').then(function(result){
-    console.log(result.data); // console feedback
+    //console.log(result.data); // console feedback
     $scope.datapersonas = result.data; // UI data
   })
 
-});
+})
+
+.controller('showPerfil', function($scope, $http, $state, $ionicPopup){
+ //función validadora de ruts
+  var Fn = {
+	validaRut : function (rutCompleto) {
+		if (!/^[0-9]+-[0-9kK]{1}$/.test( rutCompleto ))
+			return false;
+		var tmp 	= rutCompleto.split('-');
+		var digv	= tmp[1];
+		var rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+		return (Fn.dv(rut) == digv );
+	},
+	dv : function(T){
+		var M=0,S=1;
+		for(;T;T=Math.floor(T/10))
+			S=(S+T%10*(9-M++%6))%11;
+		return S?S-1:'k';
+	}
+}
+//FIN función validadora de ruts
+  $scope.data = {};
+  $http.get('https://private-4b730d-phonebooktest.apiary-mock.com/persona').then(function(result){
+    console.log(result.data); // console feedback
+    $scope.datapersonas = result.data; // UI data
+  })
+  $scope.idPersona = function(id, nombre, apellido, rut, telefono) {
+    console.log(telefono.toString().length);
+    if (telefono.toString().length < 11) {
+      val='inválido';
+    }
+    else {
+      val='válido';
+    }
+    rutval=Fn.validaRut(rut) ? 'Valido' : 'inválido';
+    $ionicPopup.alert({
+      title: nombre+" "+apellido,
+      template: '<p class="justify">Rut: '+rut+' ('+rutval+')</p><p class="justify">Teléfono: '+telefono+' ('+val+')</p>',
+      okType: 'button-df'
+
+    })
+  }
+
+    //alert('No tienes productos en Favoritos')
+
+  })
